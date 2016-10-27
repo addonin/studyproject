6621034f19f8de969e6857a1b6dc7fa8baf3ae3c
Adonin <adonin@brainacad.com>
2016-10-27 21:32:20 +0300

    Init

---
+++ src/main/java/com/brainacad/studyproject/Application.java
@@ -0,0 +1,9 @@
+package com.brainacad.studyproject;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+}
---
+++ src/test/java/com/brainacad/studyproject/ApplicationTest.java
@@ -0,0 +1,4 @@
+package com.brainacad.studyproject;
+
+public class ApplicationTest {
+}
